13f8410940c8a3af2928ea2ebe3759afa6e5a7b4
rot1024 <aayhrot@gmail.com>
2023-03-30 19:47:22 +0900

    fix(plugin): tran should enable jsonProeprties

--- plugin/src/sidebar.ts
+++ plugin/src/sidebar.ts
@@ -733,7 +733,7 @@ function createLayer(dataset: DataCatalogItem, overrides?: any) {
       url: dataset.config?.data?.[0].url ?? dataset.url,
       layers: dataset.config?.data?.[0].layers ?? dataset.layers,
       ...(format === "wms" ? { parameters: { transparent: "true", format: "image/png" } } : {}),
-      ...(["luse", "lsld", "urf", "rail"].includes(dataset.type_en) ||
+      ...(["luse", "lsld", "urf", "rail", "tran"].includes(dataset.type_en) ||
       (dataset.type_en === "tran" && format === "mvt")
         ? { jsonProperties: ["attributes"] }
         : {}),
