9983028986a1b520d8dfc8c82d2715c915d715d0
rot1024 <aayhrot@gmail.com>
2023-04-01 22:33:52 +0900

    fix(server): test fails

--- server/putil/lock_test.go
+++ server/putil/lock_test.go
@@ -10,6 +10,7 @@ import (
 func TestKeyLock(t *testing.T) {
 	k := NewKeyLock[string]()
 	m := []string{}
+	l := sync.Mutex{}
 	wg := &sync.WaitGroup{}
 	wg.Add(2)
 	k.Lock("1")
@@ -17,19 +18,29 @@ func TestKeyLock(t *testing.T) {
 	go func() {
 		k.Lock("1")
 		defer k.Unlock("1")
+
+		l.Lock()
 		m = append(m, "a")
+		l.Unlock()
+
 		wg.Done()
 	}()
 
 	go func() {
 		k.Lock("2")
 		defer k.Unlock("2")
+
+		l.Lock()
 		m = append(m, "c")
+		l.Unlock()
+
 		k.Unlock("1")
 		wg.Done()
 	}()
 
+	l.Lock()
 	m = append(m, "b")
+	l.Unlock()
 	wg.Wait()
 
 	assert.Equal(t, []string{"b", "c", "a"}, m)
