4d84894a9f51965933a5baba9bbbc4037ff33c67
KaWaite <34051327+KaWaite@users.noreply.github.com>
2023-04-03 10:42:50 +0900

    perf(plugin): speed up publishing/unpublishing datasets (#401)
    * speed up publishing
    
    * avoid refetching if failed

--- plugin/web/extensions/sidebar/modals/datacatalog/components/hooks.ts
+++ plugin/web/extensions/sidebar/modals/datacatalog/components/hooks.ts
@@ -83,15 +83,22 @@ export default () => {
         method,
         body: JSON.stringify(datasetToSave),
       });
-      if (res.status !== 200) {
-        handleDataFetch();
-        return;
+      if (res.status === 200) {
+        const resData = await res.json();
+        setData(prevData => {
+          if (!prevData) {
+            return [resData];
+          }
+          const index = prevData?.findIndex(d => d.dataID === resData.dataID);
+          if (index) {
+            const updatedData = [...prevData];
+            updatedData[index] = resData;
+            return updatedData;
+          }
+        });
       }
-      const data2 = await res.json();
-      console.log("DATA JUST SAVED: ", data2);
-      handleDataFetch(); // MAYBE UPDATE THIS LATER TO JUST UPDATE THE LOCAL VALUE
     },
-    [data, templates, backendAccessToken, backendURL, backendProjectName, handleDataFetch],
+    [data, templates, backendAccessToken, backendURL, backendProjectName],
   );
 
   const handleDatasetPublish = useCallback(
