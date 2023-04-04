30955ff572a1a577b423632e390d9ff24ae32ab8
rot1024 <aayhrot@gmail.com>
2023-04-04 17:21:44 +0900

    fix(server): cors for proxy

--- server/main.go
+++ server/main.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
-	"net/url"
 	"reflect"
 	"runtime"
 	"runtime/debug"
@@ -49,7 +48,7 @@ func main() {
 		return c.JSON(http.StatusOK, "pong")
 	}, putil.NoCacheMiddleware)
 
-	e.GET("/proxy/*", proxyHandlerFunc)
+	e.GET("/proxy/*", proxyHandlerFunc, middleware.CORS())
 
 	services := lo.Must(Services(conf))
 	serviceNames := lo.Map(services, func(s *Service, _ int) string { return s.Name })
@@ -122,46 +121,6 @@ func errorMessage(err error, log func(string, ...interface{})) (int, string) {
 	}
 
 	return code, msg
-}
-
-func proxyHandlerFunc(c echo.Context) error {
-	// Extract the target URL from the request path
-	targetPath := c.Param("*")
-
-	// This shouldn't be done by us but It'll do for now: @pyshx
-	if strings.HasPrefix(targetPath, "http:/") && len(targetPath) > 6 && targetPath[6] != '/' {
-		targetPath = "http://" + strings.TrimPrefix(targetPath, "http:/")
-	} else if strings.HasPrefix(targetPath, "https:/") && len(targetPath) > 7 && targetPath[7] != '/' {
-		targetPath = "https://" + strings.TrimPrefix(targetPath, "https:/")
-	}
-
-	targetURL, err := url.Parse(targetPath)
-	if err != nil {
-		return c.JSON(http.StatusBadRequest, map[string]string{
-			"error": "Invalid target URL",
-		})
-	}
-
-	// Define the ProxyConfig object with custom Rewrite rules and ModifyResponse function
-	proxyConfig := middleware.ProxyConfig{
-		Balancer: middleware.NewRoundRobinBalancer([]*middleware.ProxyTarget{
-			{
-				URL: targetURL,
-			},
-		}),
-	}
-
-	// Create a new middleware.ProxyWithConfig() middleware with the target URL
-	proxyMiddleware := middleware.ProxyWithConfig(proxyConfig)(func(c echo.Context) error {
-		return nil
-	})
-
-	// Invoke the proxy middleware to handle the request and return the response
-	if err := proxyMiddleware(c); err != nil {
-		return err
-	}
-
-	return nil
 }
 
 type customValidator struct {
