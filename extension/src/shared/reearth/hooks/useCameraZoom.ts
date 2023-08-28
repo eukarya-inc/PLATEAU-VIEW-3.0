058db49723ce7e0e676b69c82fa457d130c49bcd
keiya01 <keiya.s.0210@gmail.com>
2023-08-28 11:25:50 +0900

    fix zoom

--- extension/src/shared/reearth/hooks/useCameraZoom.ts
+++ extension/src/shared/reearth/hooks/useCameraZoom.ts
@@ -9,11 +9,11 @@ export function useCameraZoom(): CameraZoom {
   const camera = window.reearth.camera;
 
   const zoomOut = useCallback(() => {
-    camera?.zoomOut(-1);
+    camera?.zoomOut(1.5);
   }, [camera]);
 
   const zoomIn = useCallback(() => {
-    camera?.zoomIn(1 / 2);
+    camera?.zoomIn(1.5);
   }, [camera]);
 
   return { zoomIn, zoomOut };
