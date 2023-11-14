a5adaec43fbd65b476894507eb9a609e9951d20d
keiya01 <keiya.s.0210@gmail.com>
2023-11-14 13:30:08 +0900

    support flood model

--- extension/src/shared/view/hooks/useIsMultipleSelectableField.ts
+++ extension/src/shared/view/hooks/useIsMultipleSelectableField.ts
@@ -1,16 +1,20 @@
 import { LayerType } from "../../../prototypes/layers";
 import { BUILDING_LAYER } from "../../../prototypes/view-layers";
 import { ComponentBase } from "../../types/fieldComponents";
-import { TILESET_BUILDING_MODEL_COLOR } from "../../types/fieldComponents/3dtiles";
+import {
+  TILESET_BUILDING_MODEL_COLOR,
+  TILESET_FLOOD_MODEL_COLOR,
+} from "../../types/fieldComponents/3dtiles";
 import { OPACITY_FIELD } from "../../types/fieldComponents/general";
-import { LayerModel } from "../../view-layers";
+import { FLOOD_LAYER_TYPES, LayerModel } from "../../view-layers";
 import { ComponentAtom } from "../../view-layers/component";
 
 export const MULTIPLE_SELECTABLE_FIELDS: ComponentBase["type"][] = [
   TILESET_BUILDING_MODEL_COLOR,
+  TILESET_FLOOD_MODEL_COLOR,
   OPACITY_FIELD,
 ];
-export const MULTIPLE_SELECTABLE_TYPES: LayerType[] = [BUILDING_LAYER];
+export const MULTIPLE_SELECTABLE_TYPES: LayerType[] = [BUILDING_LAYER, ...FLOOD_LAYER_TYPES];
 
 export const useIsMultipleSelectableField = ({
   layers,
