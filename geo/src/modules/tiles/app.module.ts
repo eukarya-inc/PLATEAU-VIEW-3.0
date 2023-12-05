d701fbb7c7e25756c1992db3074dd7cb82a85104
jashan <20891087+jashanbhullar@users.noreply.github.com>
2023-12-05 13:46:33 +0530

    - Fixed the env thing

--- geo/src/modules/tiles/app.module.ts
+++ geo/src/modules/tiles/app.module.ts
@@ -8,13 +8,16 @@ import { TileAppService } from "./app.service";
 
 @Module({
   imports: [
-    TileCacheModule.forRoot({
-      // TODO: Make this come from process.env.. Facing issue with configModule
-      // process.env.TILE_CACHE_ROOT comes as undefined - https://stackoverflow.com/questions/67482900/nestjs-not-reading-environmental-variables
-      cacheRoot: "cache",
+    TileCacheModule.forRootAsync({
+      // if you don't use useFactory process.env.TILE_CACHE_ROOT will be undefined.
+      // source: https://stackoverflow.com/questions/67482900/nestjs-not-reading-environmental-variables
+      useFactory: () => {
+        return {
+          cacheRoot: process.env.TILE_CACHE_ROOT !== "" ? process.env.TILE_CACHE_ROOT : undefined,
+        };
+      },
     }),
-    // Cache disabled for now
-    // This is for browser level cache.
+    // Cache disabled for now.This is for browser level cache.
     // TODO: Make this true when everything is tested properly
     VectorTileModule.forRoot({
       disableCache: true,
