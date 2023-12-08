290c1af4afc4baabc748006520678f7c2aa254b4
keiya sasaki <keiya.s.0210@gmail.com>
2023-12-08 10:02:18 +0900

    fix(extension, geo): cache all tiles and update styles for the map (#104)

--- extension/src/shared/constants/api.ts
+++ extension/src/shared/constants/api.ts
@@ -2,3 +2,8 @@ export let GEO_API_URL: string | undefined;
 export const setGeoApiUrl = (url: string) => {
   GEO_API_URL = url;
 };
+
+export let GSI_TILE_URL: string | undefined;
+export const setGISTileURL = (url: string) => {
+  GSI_TILE_URL = url;
+};
