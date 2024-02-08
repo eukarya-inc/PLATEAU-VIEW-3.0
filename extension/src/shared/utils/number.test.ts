0e966581e6ab27e458c8569b68ae33829e449f6e
m-abe-dev <66056064+m-abe-dev@users.noreply.github.com>
2024-02-08 16:31:47 +0900

    chore: Rounded down to two decimal places (#163)

--- extension/src/shared/utils/number.test.ts
+++ extension/src/shared/utils/number.test.ts
@@ -3,6 +3,6 @@ import { expect, test } from "vitest";
 import { roundFloat } from "./number";
 
 test("roundFloat", () => {
-  expect(roundFloat(12.3450000000001)).toBe(12.34);
+  expect(roundFloat(12.3450000000001)).toBe(12.345);
   expect(roundFloat(12)).toBe(12);
 });
