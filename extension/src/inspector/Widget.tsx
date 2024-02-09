55ab2657774c824a6c21a48c7f4a44f500e526d2
airslice <icesunex@hotmail.com>
2024-02-09 18:06:08 +0800

    fix: click away listener

--- extension/src/inspector/Widget.tsx
+++ extension/src/inspector/Widget.tsx
@@ -2,11 +2,14 @@ import { memo } from "react";
 
 import { AppOverlay } from "../prototypes/view/ui-containers/AppOverlay";
 import { WidgetContext } from "../shared/context/WidgetContext";
+import { PLATEAUVIEW_INSPECTOR_DOM_ID } from "../shared/ui-components/common/ViewClickAwayListener";
 
 export const Widget = memo(function WidgetPresenter() {
   return (
     <WidgetContext>
-      <AppOverlay type="aside" width={320} height={0} />
+      <div id={PLATEAUVIEW_INSPECTOR_DOM_ID}>
+        <AppOverlay type="aside" width={320} height={0} />
+      </div>
     </WidgetContext>
   );
 });
