09e66444d70cfc3e37502cfce0f683e312c645d0
lby <icesunex@hotmail.com>
2024-02-13 09:04:44 +0800

    fix(extension): click away listener (#170)

--- extension/src/toolbar/Widget.tsx
+++ extension/src/toolbar/Widget.tsx
@@ -18,6 +18,7 @@ import { AppHeader } from "../prototypes/view/ui-containers/AppHeader";
 import { Notifications } from "../prototypes/view/ui-containers/Notifications";
 import { WidgetContext } from "../shared/context/WidgetContext";
 import { WidgetProps } from "../shared/types/widget";
+import { PLATEAUVIEW_TOOLBAR_DOM_ID } from "../shared/ui-components/common/ViewClickAwayListener";
 import { InitialLayers } from "../shared/view/containers/InitialLayers";
 import FeedBack from "../shared/view/ui-container/Feedback";
 import MyData from "../shared/view/ui-container/MyData";
@@ -55,43 +56,45 @@ export const Widget: FC<Props> = memo(function WidgetPresenter({ widget, inEdito
   useSelectSketchFeature();
 
   return (
-    <WidgetContext
-      geoUrl={widget.property.default.geoURL}
-      gsiTileURL={widget.property.default.gsiTileURL}
-      plateauUrl={widget.property.default.plateauURL}
-      catalogUrl={widget.property.default.catalogURL}
-      catalogURLForAdmin={widget.property.default.catalogURLForAdmin}
-      projectId={widget.property.default.projectName}
-      plateauToken={widget.property.default.plateauAccessToken}
-      googleStreetViewAPIKey={widget.property.default.googleStreetViewAPIKey}
-      inEditor={inEditor}
-      customPrimaryColor={widget.property.appearance?.primaryColor}
-      customLogo={widget.property.appearance?.logo}>
-      <InitializeApp />
-      <AppFrame header={<AppHeader />} />
-      {/* TODO(ReEarth): Support initial layer loading(Splash screen) */}
-      <Loading />
-      {/* <Suspense>
+    <div id={PLATEAUVIEW_TOOLBAR_DOM_ID}>
+      <WidgetContext
+        geoUrl={widget.property.default.geoURL}
+        gsiTileURL={widget.property.default.gsiTileURL}
+        plateauUrl={widget.property.default.plateauURL}
+        catalogUrl={widget.property.default.catalogURL}
+        catalogURLForAdmin={widget.property.default.catalogURLForAdmin}
+        projectId={widget.property.default.projectName}
+        plateauToken={widget.property.default.plateauAccessToken}
+        googleStreetViewAPIKey={widget.property.default.googleStreetViewAPIKey}
+        inEditor={inEditor}
+        customPrimaryColor={widget.property.appearance?.primaryColor}
+        customLogo={widget.property.appearance?.logo}>
+        <InitializeApp />
+        <AppFrame header={<AppHeader />} />
+        {/* TODO(ReEarth): Support initial layer loading(Splash screen) */}
+        <Loading />
+        {/* <Suspense>
         <SuspendUntilTilesLoaded
           initialTileCount={35}
           remainingTileCount={30}
           onComplete={handleTilesLoadComplete}> */}
-      <LayersRenderer components={layerComponents} />
-      {/* </SuspendUntilTilesLoaded>
+        <LayersRenderer components={layerComponents} />
+        {/* </SuspendUntilTilesLoaded>
       </Suspense> */}
-      <Environments />
-      <ToolMachineEvents />
-      <Notifications />
-      <InitialLayers />
-      <SelectionCoordinator />
-      <ScreenSpaceSelection />
-      <HighlightedAreas />
-      <ReverseGeocoding />
-      <PedestrianTool />
-      <SketchTool />
-      <MyData />
-      <AutoRotateCamera />
-      <FeedBack />
-    </WidgetContext>
+        <Environments />
+        <ToolMachineEvents />
+        <Notifications />
+        <InitialLayers />
+        <SelectionCoordinator />
+        <ScreenSpaceSelection />
+        <HighlightedAreas />
+        <ReverseGeocoding />
+        <PedestrianTool />
+        <SketchTool />
+        <MyData />
+        <AutoRotateCamera />
+        <FeedBack />
+      </WidgetContext>
+    </div>
   );
 });
