b4eee77306884a71c5d9f2b1d38ddf10baf61cfa
airslice <icesunex@hotmail.com>
2024-02-20 17:53:44 +0800

    refactor: set screen space camera options

--- extension/src/prototypes/view/containers/ScreenSpaceCamera.tsx
+++ extension/src/prototypes/view/containers/ScreenSpaceCamera.tsx
@@ -1,5 +1,5 @@
 import { useAtomValue } from "jotai";
-import { useEffect, useRef } from "react";
+import { useEffect, useMemo, useRef } from "react";
 
 import { ScreenSpaceCameraControllerOptions } from "../../../shared/reearth/types";
 import { setView } from "../../../shared/reearth/utils";
@@ -9,51 +9,61 @@ import { KeyboardHandlers } from "./KeyboardHandlers";
 
 type ScreenSpaceCameraProps = {
   tiltByRightButton: boolean;
+  minimumZoomDistance?: number;
+  maximumZoomDistance?: number;
 };
-export const ScreenSpaceCamera = ({ tiltByRightButton = false }: ScreenSpaceCameraProps) => {
+export const ScreenSpaceCamera = ({
+  tiltByRightButton = false,
+  minimumZoomDistance = 1.5,
+  maximumZoomDistance = Infinity,
+}: ScreenSpaceCameraProps) => {
   const useKeyboard = useAtomValue(enableKeyboardCameraControlAtom);
 
-  const screenSpaceCameraControllerOptions: ScreenSpaceCameraControllerOptions = {
-    zoomEventTypes: [],
-    tiltEventTypes: [],
-    maximumZoomDistance: Infinity,
-    minimumZoomDistance: 1.5,
-    enableCollisionDetection: !useKeyboard,
-    lookEventTypes: [
-      "left_drag",
-      {
-        eventType: "left_drag",
-        modifier: "ctrl",
-      },
-      {
-        eventType: "left_drag",
-        modifier: "shift",
-      },
-    ],
-  };
+  const optionsWhenUseKeyboard: ScreenSpaceCameraControllerOptions = useMemo(
+    () => ({
+      zoomEventTypes: [],
+      rotateEventTypes: [],
+      tiltEventTypes: [],
+      lookEventTypes: [
+        "left_drag",
+        {
+          eventType: "left_drag",
+          modifier: "ctrl",
+        },
+        {
+          eventType: "left_drag",
+          modifier: "shift",
+        },
+      ],
+      maximumZoomDistance,
+      minimumZoomDistance,
+      enableCollisionDetection: !useKeyboard,
+    }),
+    [minimumZoomDistance, maximumZoomDistance, useKeyboard],
+  );
 
-  const tiltByRightButtonOption: ScreenSpaceCameraControllerOptions = {
-    zoomEventTypes: ["middle_drag", "wheel", "pinch"],
-    rotateEventTypes: ["left_drag"],
-    tiltEventTypes: [
-      "right_drag",
-      "pinch",
-      {
-        eventType: "left_drag",
-        modifier: "ctrl",
-      },
-      {
-        eventType: "right_drag",
-        modifier: "ctrl",
-      },
-    ],
-    lookEventTypes: [
-      {
-        eventType: "left_drag",
-        modifier: "shift",
-      },
-    ],
-  };
+  const optionsWhenTiltByRightButton: ScreenSpaceCameraControllerOptions = useMemo(
+    () => ({
+      zoomEventTypes: ["middle_drag", "wheel", "pinch"],
+      rotateEventTypes: ["left_drag"],
+      tiltEventTypes: [
+        "right_drag",
+        "pinch",
+        {
+          eventType: "left_drag",
+          modifier: "ctrl",
+        },
+        {
+          eventType: "right_drag",
+          modifier: "ctrl",
+        },
+      ],
+      maximumZoomDistance,
+      minimumZoomDistance,
+      enableCollisionDetection: !useKeyboard,
+    }),
+    [minimumZoomDistance, maximumZoomDistance, useKeyboard],
+  );
 
   const cameraPotion = window?.reearth?.camera?.position;
   const animationFrameRef = useRef<number>(0);
@@ -80,11 +90,15 @@ export const ScreenSpaceCamera = ({ tiltByRightButton = false }: ScreenSpaceCame
     };
   }, [cameraPotion, useKeyboard]);
 
-  if (useKeyboard) {
-    window?.reearth?.camera?.overrideScreenSpaceController(screenSpaceCameraControllerOptions);
-  } else if (tiltByRightButton) {
-    window?.reearth?.camera?.overrideScreenSpaceController(tiltByRightButtonOption);
-  } else return window?.reearth?.camera?.overrideScreenSpaceController() ?? null;
+  useEffect(() => {
+    if (useKeyboard) {
+      window?.reearth?.camera?.overrideScreenSpaceController(optionsWhenUseKeyboard);
+    } else if (tiltByRightButton) {
+      window?.reearth?.camera?.overrideScreenSpaceController(optionsWhenTiltByRightButton);
+    } else {
+      window?.reearth?.camera?.overrideScreenSpaceController();
+    }
+  }, [useKeyboard, tiltByRightButton, optionsWhenUseKeyboard, optionsWhenTiltByRightButton]);
 
   return useKeyboard ? <KeyboardHandlers isMoving={useKeyboard} /> : null;
 };
