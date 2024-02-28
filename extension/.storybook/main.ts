0e28fc206055801babf7de2dee5f0a1ab0b45cf9
jashan <20891087+jashanbhullar@users.noreply.github.com>
2024-02-28 19:19:01 +0530

    - upgraded storybook version - fixed storybook broken implementation

--- extension/.storybook/main.ts
+++ extension/.storybook/main.ts
@@ -1,11 +1,39 @@
 import type { StorybookConfig } from "@storybook/react-vite";
+import { mergeConfig } from "vite";
+
+// 7.6.17
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.story.@(js|jsx|mjs|ts|tsx)"],
-  addons: ["@storybook/addon-essentials"],
+  addons: ["@storybook/addon-essentials", "@storybook/addon-themes"],
   framework: {
     name: "@storybook/react-vite",
     options: {},
+  },
+  core: {
+    disableTelemetry: true,
+  },
+  viteFinal(config, { configType }) {
+    return mergeConfig(config, {
+      define: {
+        "process.env.QTS_DEBUG": "false", // quickjs-emscripten
+      },
+
+      build:
+        configType === "PRODUCTION"
+          ? {
+              // https://github.com/storybookjs/builder-vite/issues/409
+              minify: false,
+              sourcemap: false,
+            }
+          : {},
+      server: {
+        watch: {
+          // https://github.com/storybookjs/storybook/issues/22253#issuecomment-1673229400
+          ignored: ["**/.env"],
+        },
+      },
+    });
   },
   docs: {
     autodocs: "tag",
