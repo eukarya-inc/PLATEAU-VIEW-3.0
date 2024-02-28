360aee418f8aca74dc6dc4ebc45a2bbbb2f88153
KaWaite <34051327+KaWaite@users.noreply.github.com>
2024-02-29 08:34:09 +0900

    Update extension/src/prototypes/view/ui-containers/SelectionPanel.tsx
    Co-authored-by: keiya sasaki <keiya.s.0210@gmail.com>

--- extension/src/prototypes/view/ui-containers/SelectionPanel.tsx
+++ extension/src/prototypes/view/ui-containers/SelectionPanel.tsx
@@ -47,7 +47,6 @@ export const SelectionPanel: FC = () => {
   if (selectionGroups.length === 1) {
     const [selectionGroup] = selectionGroups;
     const { type, subtype } = selectionGroup;
-    console.log("ASDFSDF", selectionGroup);
     switch (type) {
       case LAYER_SELECTION:
         switch (subtype) {
