efb668c1d7847264a4a262b324a7f8dd81ccebfc
KaWaite <34051327+KaWaite@users.noreply.github.com>
2024-03-05 15:22:08 +0900

    fix(extension): same typed datasets sharing same colorset (#226)

--- extension/src/prototypes/datasets/colorSets/landslideRiskColorSet.ts
+++ extension/src/prototypes/datasets/colorSets/landslideRiskColorSet.ts
@@ -4,22 +4,6 @@ import { atomsWithQualitativeColorSet } from "../atomsWithQualitativeColorSet";
 
 import { LANDSLIDE_RISK_TYPE_CODES } from "./constants";
 
-// Old values from Takram. Doesn't seem to be used
-export const landSlideRiskColorSet = atomsWithQualitativeColorSet({
-  id: "landslide_risk",
-  name: "土砂災害警戒区域",
-  // Colors inherited from VIEW 2.0.
-  // List of codes: https://www.mlit.go.jp/plateaudocument/#toc4_09_04
-  // prettier-ignore
-  colors: [
-    { value: '1', color: '#ffed4c', name: '土砂災害警戒区域（指定済）' },
-    { value: '2', color: '#fb684c', name: '土砂災害特別警戒区域（指定済）' },
-    { value: '3', color: '#ffed4c', name: '土砂災害警戒区域（指定前）' },
-    { value: '4', color: '#fb684c', name: '土砂災害特別警戒区域（指定前）' }
-  ],
-});
-
-// Newest values from Eukarya currently in use
 export const LANDSLIDE_RISK_COLORS = [
   {
     value: LANDSLIDE_RISK_TYPE_CODES[0],
@@ -33,7 +17,9 @@ export const LANDSLIDE_RISK_COLORS = [
   },
 ];
 
-export const landslideRiskColorSet = atomsWithQualitativeColorSet({
-  name: "地すべり",
-  colors: LANDSLIDE_RISK_COLORS,
-});
+export const landslideRiskColorSet = (id: string, name: string) =>
+  atomsWithQualitativeColorSet({
+    id: `${id}-landslide_risk`,
+    name,
+    colors: LANDSLIDE_RISK_COLORS,
+  });
