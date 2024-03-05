d9174df070319481278714cd248fbd076fe88824
mkumbobeaty <charsbeaty@gmail.com>
2024-03-05 12:49:27 +0300

    refactor init camera

--- extension/src/prototypes/view/ui-containers/CompassButton.tsx
+++ extension/src/prototypes/view/ui-containers/CompassButton.tsx
@@ -1,5 +1,5 @@
 import { useAtom } from "jotai";
-import { FC, useCallback, useEffect, useState } from "react";
+import { FC, useCallback, useEffect, useRef, useState } from "react";
 
 import { useCamera } from "../../../shared/reearth/hooks";
 import { AppIconButton, CompassIcon } from "../../ui-components";
@@ -10,20 +10,21 @@ export const CompassButton: FC = () => {
 
   const { getCameraPosition, flyTo } = useCamera();
   const camera = getCameraPosition();
+  const [autoRotateCamera, setAutoRotateCameraAtom] = useAtom(autoRotateCameraAtom);
+  const initialCamera = useRef(camera);
+
   const radianToDegree = useCallback((rad: number) => rad * (180 / Math.PI), []);
 
   useEffect(() => {
     if (camera?.heading) {
       setRotationAngle(360 - radianToDegree(camera?.heading));
     }
-  }, [camera?.heading, radianToDegree]);
-
-  const [autoRotateCamera, setAutoRotateCameraAtom] = useAtom(autoRotateCameraAtom);
+  }, [camera, camera?.heading, radianToDegree]);
 
   const handleClick = useCallback(() => {
-    flyTo({ ...camera, heading: 0 });
+    if (initialCamera.current) flyTo(initialCamera.current);
     if (autoRotateCamera) setAutoRotateCameraAtom(value => !value);
-  }, [autoRotateCamera, camera, flyTo, setAutoRotateCameraAtom]);
+  }, [autoRotateCamera, flyTo, initialCamera, setAutoRotateCameraAtom]);
 
   return (
     <AppIconButton title="コンパス" onClick={handleClick}>
