a620d0f574087c0f22695d406112c1b5ca89b140
jashan <20891087+jashanbhullar@users.noreply.github.com>
2024-03-05 19:01:20 +0530

    - tried to get all the layers but shows undefined

--- extension/src/prototypes/view/ui-containers/AppHeader.tsx
+++ extension/src/prototypes/view/ui-containers/AppHeader.tsx
@@ -1,7 +1,9 @@
 import { IconButton, useMediaQuery, useTheme } from "@mui/material";
-import { useAtom, useAtomValue } from "jotai";
-import { type FC } from "react";
+import { useAtom, useAtomValue, useSetAtom } from "jotai";
+import { useState, type FC, useEffect } from "react";
 
+import { SharedRootLayer, getSharedRootLayersAtom } from "../../../shared/states/share";
+import { isAppReadyAtom } from "../../../shared/view/state/app";
 import ShareModal from "../../../shared/view/ui-container/ShareModal";
 import { AppBar, PaperPlaneTilt, Space } from "../../ui-components";
 import { hideAppOverlayAtom, showShareModalAtom } from "../states/app";
@@ -19,9 +21,23 @@ export const AppHeader: FC = () => {
   const [showShareModal, setShowShareModal] = useAtom(showShareModalAtom);
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("mobile"));
+  const getSharedRootLayers = useSetAtom(getSharedRootLayersAtom);
+  const [sharedRootLayers, setSharedRootLayers] = useState<SharedRootLayer[] | undefined>();
+  const isAppReady = useAtomValue(isAppReadyAtom);
+
+  useEffect(() => {
+    (async () => {
+      if (!isAppReady) return;
+      const layers = await getSharedRootLayers();
+      setSharedRootLayers(layers);
+    })();
+  }, [isAppReady, getSharedRootLayers]);
   if (hidden) {
     return null;
   }
+
+  console.log(sharedRootLayers);
+
   return (
     <AppBar>
       <MainMenuButton />
