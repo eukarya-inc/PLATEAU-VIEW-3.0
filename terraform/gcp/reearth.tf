ef969407dd7c8ffaa363c3bf321f4396876c54e3
KeisukeYamashita <19yamashita15@gmail.com>
2024-03-16 13:15:22 +0000

    fix: propagate gcp region to the reearth terraform module
    Signed-off-by: KeisukeYamashita <19yamashita15@gmail.com>

--- terraform/gcp/reearth.tf
+++ terraform/gcp/reearth.tf
@@ -6,6 +6,7 @@ module "reearth" {
   dns_managed_zone_name      = google_dns_managed_zone.zone.name
   domain                     = var.domain
   gcp_project_id             = var.gcp_project_id
+  gcp_region                 = var.gcp_region
   mongodb_connection_string  = var.mongodb_connection_string
   reearth_marketplace_secret = var.reearth_marketplace_secret
   reearth_web_config         = var.reearth_web_config
