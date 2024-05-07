43dfb18e95223bfdc9bbfb9148d7fad243bf3a86
keiya sasaki <keiya.s.0210@gmail.com>
2024-05-07 15:28:28 +0900

    Revert "fix(extension): use show property to toggle Japan polygon" (#311)

--- extension/src/shared/view/containers/JapanPlateauPolygon.tsx
+++ extension/src/shared/view/containers/JapanPlateauPolygon.tsx
@@ -1,58 +1,44 @@
-import { FC, useCallback, useMemo, useRef, useState } from "react";
+import { FC, useCallback, useState } from "react";
 
 import { useCamera, useReEarthEvent } from "../../reearth/hooks";
 import { PolygonAppearances, PolygonLayer } from "../../reearth/layers";
 
 const CAMERA_ZOOM_LEVEL_HEIGHT = 300000;
+const appererances: PolygonAppearances = {
+  resource: {
+    fill: "rgba(0,190,190, 0.2)",
+    stroke: "rgba(0, 190, 190, 1)",
+    strokeWidth: 1,
+    hideIndicator: true,
+    clampToGround: true,
+  },
+  polygon: {
+    classificationType: "terrain",
+    fill: true,
+    fillColor: "rgba(0, 190, 190, 0.2)",
+    heightReference: "clamp",
+    hideIndicator: true,
+    selectedFeatureColor: "rgba(0, 190, 190, 0.4)",
+  },
+};
 const JapanPlateauPolygon: FC = () => {
   const { getCameraPosition } = useCamera();
   const [visible, setVisible] = useState(false);
-  const initializedRef = useRef(false);
 
-  const timer = useRef<number>();
   const updateVisibility = useCallback(() => {
-    if (timer.current) return;
-
-    timer.current = window.setTimeout(() => {
-      const camera = getCameraPosition();
-      if (camera?.height && camera.height >= CAMERA_ZOOM_LEVEL_HEIGHT) {
-        initializedRef.current = true;
-        setVisible(true);
-      } else {
-        setVisible(false);
-      }
-      timer.current = undefined;
-    }, 300);
+    const camera = getCameraPosition();
+    if (camera?.height && camera.height >= CAMERA_ZOOM_LEVEL_HEIGHT) {
+      setVisible(true);
+    } else {
+      setVisible(false);
+    }
   }, [getCameraPosition]);
 
   useReEarthEvent("cameramove", updateVisibility);
 
-  const appearances: PolygonAppearances = useMemo(
-    () => ({
-      resource: {
-        fill: "rgba(0,190,190, 0.2)",
-        stroke: "rgba(0, 190, 190, 0.2)",
-        strokeWidth: 1,
-        hideIndicator: true,
-        clampToGround: true,
-        show: visible,
-      },
-      polygon: {
-        classificationType: "terrain",
-        fill: true,
-        fillColor: "rgba(0, 190, 190, 0.2)",
-        heightReference: "clamp",
-        hideIndicator: true,
-        selectedFeatureColor: "rgba(0, 190, 190, 0.4)",
-        show: visible,
-      },
-    }),
-    [visible],
-  );
+  if (!visible) return null;
 
-  if (!initializedRef.current) return null;
-
-  return <PolygonLayer appearances={appearances} />;
+  return <PolygonLayer appearances={appererances} />;
 };
 
 export default JapanPlateauPolygon;
